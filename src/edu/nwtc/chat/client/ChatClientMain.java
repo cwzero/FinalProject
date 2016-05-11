537d79311afa352a18fc0054d86a4f57450455d0
Eric Gjerde <gjerdeeric@gmail.com>
2016-05-11 17:11:05 -0500

    Add Font

--- src/edu/nwtc/chat/client/ChatClientMain.java
+++ src/edu/nwtc/chat/client/ChatClientMain.java
@@ -1,9 +1,23 @@
 package edu.nwtc.chat.client;
 
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.JFrame;
 
 public class ChatClientMain {
 	public static void main(String[] args) {
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		try {
+			env.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("BuxtonSketch.ttf")));
+		} catch (FontFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		JFrame chatWindow  = new JFrame();
 		chatWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		chatWindow.setSize(600, 600);
